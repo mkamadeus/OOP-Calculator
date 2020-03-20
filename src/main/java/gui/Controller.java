d3c541f2d61e780bf236356fea2340f1511a3d0d
Matthew Kevin Amadeus <mkamadeus.mka@gmail.com>
2020-03-21 03:24:14 +0700

    Added operator functions for keypress

--- src/main/java/gui/Controller.java
+++ src/main/java/gui/Controller.java
@@ -4,14 +4,14 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Control;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.input.*;
 
 public class Controller {
 
     @FXML
     private Button buttonClear;
+    @FXML
+    private Button button0;
     @FXML
     private Button button1;
     @FXML
@@ -32,10 +32,28 @@ public class Controller {
     private Button button9;
 
     @FXML
+    private Button buttonPlus;
+    @FXML
+    private Button buttonMinus;
+    @FXML
+    private Button buttonMultiply;
+    @FXML
+    private Button buttonDivide;
+    @FXML
+    private Button buttonParOpen;
+    @FXML
+    private Button buttonParClose;
+
+
+    @FXML
+    private TextField equationDisplay;
+    @FXML
     private TextField numberDisplay;
 
+    // Array to store number buttons reference
     private Button[] numberButtons;
     private KeyCode[] numberKeyCodes = {
+            KeyCode.DIGIT0,
             KeyCode.DIGIT1,
             KeyCode.DIGIT2,
             KeyCode.DIGIT3,
@@ -47,47 +65,100 @@ public class Controller {
             KeyCode.DIGIT9
     };
 
+    // Array to store operator buttons reference
+    private Button[] operatorButtons;
+    private KeyCombination[] operatorKeyCodes = {
+            new KeyCodeCombination(KeyCode.EQUALS, KeyCombination.SHIFT_DOWN),
+            new KeyCodeCombination(KeyCode.MINUS),
+            new KeyCodeCombination(KeyCode.DIGIT8, KeyCombination.SHIFT_DOWN),
+            new KeyCodeCombination(KeyCode.SLASH),
+            new KeyCodeCombination(KeyCode.DIGIT9, KeyCombination.SHIFT_DOWN),
+            new KeyCodeCombination(KeyCode.DIGIT0, KeyCombination.SHIFT_DOWN)
+    };
+
+    @FXML
     public void initialize()
     {
-        numberButtons = new Button[9];
-        numberButtons[0] = button1;
-        numberButtons[1] = button2;
-        numberButtons[2] = button3;
-        numberButtons[3] = button4;
-        numberButtons[4] = button5;
-        numberButtons[5] = button6;
-        numberButtons[6] = button7;
-        numberButtons[7] = button8;
-        numberButtons[8] = button9;
+        numberButtons = new Button[10];
+        numberButtons[0] = button0;
+        numberButtons[1] = button1;
+        numberButtons[2] = button2;
+        numberButtons[3] = button3;
+        numberButtons[4] = button4;
+        numberButtons[5] = button5;
+        numberButtons[6] = button6;
+        numberButtons[7] = button7;
+        numberButtons[8] = button8;
+        numberButtons[9] = button9;
+
+        operatorButtons = new Button[6];
+        operatorButtons[0] = buttonPlus;
+        operatorButtons[1] = buttonMinus;
+        operatorButtons[2] = buttonMultiply;
+        operatorButtons[3] = buttonDivide;
+        operatorButtons[4] = buttonParOpen;
+        operatorButtons[5] = buttonParClose;
+
     }
 
     @FXML
-    private void handleOnKeyPress(KeyEvent event) {
+    private void handleOnKeyPressed(KeyEvent event) {
+        System.out.println(event);
+
+        // Check for operators
+        for(int i = 0; i < 6 ; i++) if(operatorKeyCodes[i].match(event))
+        {
+            operatorButtons[i].getStyleClass().removeAll("buttons");
+            operatorButtons[i].getStyleClass().add("buttons:pressed");
+
+            equationDisplay.setText(equationDisplay.getText() + numberDisplay.getText() + operatorButtons[i].getText());
+            numberDisplay.setText("");
+
+            return;
+        }
+
         // Check for number key press
-        for(int i=0;i<9;i++)
+        for(int i = 0; i < 10; i++)
         {
             if(event.getCode() == numberKeyCodes[i])
             {
                 numberButtons[i].getStyleClass().removeAll("buttons");
                 numberButtons[i].getStyleClass().add("buttons:pressed");
-                numberDisplay.setText(numberDisplay.getText() + (i+1));
+
+                // If input is 0 and currently no text...
+                if(i!=0 || !("".equals(numberDisplay.getText()))) {
+                    numberDisplay.setText(numberDisplay.getText() + i);
+                }
+
                 return;
             }
         }
 
-        // Check for DELETE key press
+        // Check for DELETE Key
         if(event.getCode() == KeyCode.DELETE)
         {
             buttonClear.getStyleClass().removeAll("buttons");
             buttonClear.getStyleClass().add("buttons:pressed");
             numberDisplay.setText("");
+
+            return;
         }
+
     }
 
     @FXML
-    private void handleOnKeyRelease(KeyEvent event) {
+    private void handleOnKeyReleased(KeyEvent event) {
+        // Check for operators
+        for(int i = 0; i < 6; i++) if(operatorKeyCodes[i].match(event))
+        {
+            operatorButtons[0].getStyleClass().removeAll("buttons:pressed");
+            operatorButtons[0].getStyleClass().add("buttons");
+
+            return;
+        }
+
         // Check for number key press
-        for (int i = 0; i < 9; i++) {
+        for (int i = 0; i < 10; i++) {
             if (event.getCode() == numberKeyCodes[i]) {
                 numberButtons[i].getStyleClass().removeAll("buttons:pressed");
                 numberButtons[i].getStyleClass().add("buttons");
@@ -95,12 +166,15 @@ public class Controller {
             }
         }
 
-        // Check for DELETE key press
+        // Check for DELETE key
         if(event.getCode() == KeyCode.DELETE)
         {
             buttonClear.getStyleClass().removeAll("buttons:pressed");
             buttonClear.getStyleClass().add("buttons");
+
+            return;
         }
+
 
     }
 
