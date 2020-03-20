852bb8607c7e7edcc1c17134b3c45293d4d3a591
Matthew Kevin Amadeus <mkamadeus.mka@gmail.com>
2020-03-20 13:56:38 +0700

    Added calc typing using keyboard

--- src/main/java/gui/Calculator.java
+++ src/main/java/gui/Calculator.java
@@ -1,110 +1,28 @@
 package gui;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
 import javafx.stage.Stage;
-import javafx.event.ActionEvent; 
-import javafx.event.EventHandler; 
+
 
 public class Calculator extends Application
 {
-    private TextField displayValue;
-
-    public HBox displayBox()
-    {
-        // Create new HBox
-        HBox displayContainer = new HBox();
-
-        displayValue.setEditable(false);
-        displayValue.setPadding(new Insets(12,12,12,12));
-        displayValue.setStyle("-fx-background-color: rgba(0,0,0,0), #ffffff;");
-        displayValue.setText("");
-
-        displayContainer.setStyle("-fx-background-color:#eeeeee");
-        displayContainer.setPadding(new Insets(12,12,12,12));
-        HBox.setHgrow(displayValue, Priority.ALWAYS);
-        displayContainer.getChildren().addAll(displayValue);
-
-        return displayContainer;
-        
-    }
-
-    public GridPane calculatorButtons()
-    {
-        GridPane buttonContainer = new GridPane();
-
-        GridPane.setColumnSpan(new Button(), 5);
-        GridPane.setRowSpan(new Button(), 5);
-        
-        buttonContainer.setPadding(new Insets(10));
-        buttonContainer.setHgap(10);
-        buttonContainer.setVgap(10);
-        
-        
-        // Button text contents
-        String buttonText[][] = {
-            {"sin", "cos", "tan", "CLR"},
-            {"(", ")", "sqrt", "^"},
-            {"7", "8", "9", "*"},
-            {"4", "5", "6", "/"},
-            {"1", "2", "3", "-"},
-            {"0", ".", "=", "+"},
-            {"ANS", "MC", "MR", "<="},
-        };
-        
-        for(int i=0;i<7;i++)
-        {
-            HBox h = new HBox();
-            Button b;
-            for(int j=0;j<4;j++)
-            {
-                b = new Button(buttonText[i][j]);
-
-                final String s = buttonText[i][j];
-                EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() {
-                    public void handle(ActionEvent e)
-                    {
-                        String tmp = displayValue.getText();
-                        tmp += s;
-                        displayValue.setText(tmp);
-                    }
-                };
-
-                b.setMinWidth(50);
-                b.setOnAction(event);
-                h.getChildren().add(b);
-                HBox.setHgrow(b, Priority.ALWAYS);
-            }
-            buttonContainer.add(h,0,i);
-            
-        }
-
-        return buttonContainer;
-    }
-
     @Override
     public void start(Stage stage) throws Exception
     {
+        // Load FXML file
         FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("layout.fxml"));
 
-//        loader.setController(new Controller());
-
+        // Load outer outermost element from the FXML file
         Parent root = loader.load();
 
+        // Setup new scene
         Scene scene = new Scene(root, 350, 700);
 
-        stage.setTitle("FXML Welcome");
+        // Set stage to the scene
+        stage.setTitle("Calculator");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/gui/Controller.java
+++ src/main/java/gui/Controller.java
@@ -1,12 +1,114 @@
 package gui;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Control;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 
 public class Controller {
+
     @FXML
-    private void handleOnNumberButtonsPressed(KeyEvent event) {
-        System.out.println("Pressed key text: " + event.getText());
-        System.out.println("Pressed key code: " + event.getCode());
+    private Button buttonClear;
+    @FXML
+    private Button button1;
+    @FXML
+    private Button button2;
+    @FXML
+    private Button button3;
+    @FXML
+    private Button button4;
+    @FXML
+    private Button button5;
+    @FXML
+    private Button button6;
+    @FXML
+    private Button button7;
+    @FXML
+    private Button button8;
+    @FXML
+    private Button button9;
+
+    @FXML
+    private TextField numberDisplay;
+
+    private Button[] numberButtons;
+    private KeyCode[] numberKeyCodes = {
+            KeyCode.DIGIT1,
+            KeyCode.DIGIT2,
+            KeyCode.DIGIT3,
+            KeyCode.DIGIT4,
+            KeyCode.DIGIT5,
+            KeyCode.DIGIT6,
+            KeyCode.DIGIT7,
+            KeyCode.DIGIT8,
+            KeyCode.DIGIT9
+    };
+
+    public void initialize()
+    {
+        numberButtons = new Button[9];
+        numberButtons[0] = button1;
+        numberButtons[1] = button2;
+        numberButtons[2] = button3;
+        numberButtons[3] = button4;
+        numberButtons[4] = button5;
+        numberButtons[5] = button6;
+        numberButtons[6] = button7;
+        numberButtons[7] = button8;
+        numberButtons[8] = button9;
     }
+
+    @FXML
+    private void handleOnKeyPress(KeyEvent event) {
+        // Check for number key press
+        for(int i=0;i<9;i++)
+        {
+            if(event.getCode() == numberKeyCodes[i])
+            {
+                numberButtons[i].getStyleClass().removeAll("buttons");
+                numberButtons[i].getStyleClass().add("buttons:pressed");
+                numberDisplay.setText(numberDisplay.getText() + (i+1));
+                return;
+            }
+        }
+
+        // Check for DELETE key press
+        if(event.getCode() == KeyCode.DELETE)
+        {
+            buttonClear.getStyleClass().removeAll("buttons");
+            buttonClear.getStyleClass().add("buttons:pressed");
+            numberDisplay.setText("");
+        }
+    }
+
+    @FXML
+    private void handleOnKeyRelease(KeyEvent event) {
+        // Check for number key press
+        for (int i = 0; i < 9; i++) {
+            if (event.getCode() == numberKeyCodes[i]) {
+                numberButtons[i].getStyleClass().removeAll("buttons:pressed");
+                numberButtons[i].getStyleClass().add("buttons");
+                return;
+            }
+        }
+
+        // Check for DELETE key press
+        if(event.getCode() == KeyCode.DELETE)
+        {
+            buttonClear.getStyleClass().removeAll("buttons:pressed");
+            buttonClear.getStyleClass().add("buttons");
+        }
+
+    }
+
+    @FXML
+    private void handleOnNumberClick(MouseEvent mouseEvent)
+    {
+        String text = ((Button)mouseEvent.getSource()).getText();
+        numberDisplay.setText(numberDisplay.getText() + text);
+    }
+
 }
